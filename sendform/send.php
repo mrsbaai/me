3e201494bb25eb1ad442f73d0a78bb33c007b22d
Desk <abdelilah.sbaai@gmail.com>
2018-10-21 18:59:02 +0100

    content

--- sendform/send.php
+++ sendform/send.php
@@ -38,6 +38,11 @@ if ($conn->connect_error) {
 
 $sql = "INSERT INTO people (first_name, last_name, email, address, type, phone, front, back) VALUES ('$firstname', '$lastname', '$email', '$firstname', '$address', '$type', '$phone', '$front', '$back')";
 
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 $conn->close();
 
 
