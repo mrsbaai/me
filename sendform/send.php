03881a3e3edcf1672dcbd4a7cf650e08c26f0792
Desk <abdelilah.sbaai@gmail.com>
2018-10-21 18:01:17 +0100

    content

--- sendform/send.php
+++ sendform/send.php
@@ -1,12 +1,12 @@
 
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["front"]["name"]);
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["front"]["tmp_name"]);
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
