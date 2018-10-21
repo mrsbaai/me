ed632c0b1bb5dad7aac36aef67595cc943c3f8dd
Desk <abdelilah.sbaai@gmail.com>
2018-10-21 17:49:54 +0100

    content

--- sendform/send.php
+++ sendform/send.php
@@ -1,23 +1,6 @@
 <?php
-  
-
-$servername = "localhost";
-$username = "forge";
-$password = "ZUMeAhYa2FsU8GITdoHs";
-$dbname = "forge";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-//$sql = "INSERT INTO people (first_name, last_name, email) VALUES ('John', 'Doe', 'john@example.com')";
-
-$conn->close();
-print_r($_FILES);
+  echo 'file count=', count($_FILES),"\n";
+var_dump($_FILES);
+echo "\n";
 
 ?>
