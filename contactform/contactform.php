56d0282738b5d2a36c7011727dfb1ad9db6a3fe2
Desk <abdelilah.sbaai@gmail.com>
2018-10-23 12:21:05 +0100

    contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,6 +1,35 @@
+
 <?php
 
 
-  //echo  $_POST['name'];
-  echo "Merci pour vorte message!";
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$servername = "localhost";
+$username = "forge";
+$password = "ZUMeAhYa2FsU8GITdoHs";
+$dbname = "forge";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+     	echo ("<script LANGUAGE='JavaScript'>
+    window.alert('Erreur!');
+    window.location.href='marocemploi.live';
+    </script>");
+}
+
+
+$sql = "INSERT INTO people (first_name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+$v= $conn->query($sql);
+
+$conn->close();
+ echo "Merci pour vorte message!";
+
 ?>
+
