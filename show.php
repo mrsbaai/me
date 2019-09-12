8fdb3b971e63e997d73681f1ba9bfb9b5fde545f
mrsbaai <abdelilah.sbaai@gmail.com>
2019-09-12 15:29:01 +0100

    Update show.php

--- show.php
+++ show.php
@@ -3,9 +3,9 @@
 $files = glob("uploads/*.*");
 for ($i=1; $i<count($files); $i++)
 {
-	//$num = $files[$i];
-	//print $num."<br />";
-	//echo '<img src="'.$num.'" alt="random image" />'."<br /><br />";
+	$num = $files[$i];
+	print $num."<br />";
+	echo '<img src="'.$num.'" alt="random image" />'."<br /><br />";
 }
 
 
