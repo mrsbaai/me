056715503b27156a214b5e25c840ad59de7a03a8
mrsbaai <abdelilah.sbaai@gmail.com>
2019-09-12 15:02:50 +0100

    Update show.php

--- show.php
+++ show.php
@@ -1,10 +1,20 @@
 
 <?php
-$files = glob("uploads/*.*");
-for ($i=1; $i<count($files); $i++)
-{
-	$num = $files[$i];
-	print $num."<br />";
-	echo '<img src="'.$num.'" alt="random image" />'."<br /><br />";
+//$files = glob("uploads/*.*");
+//for ($i=1; $i<count($files); $i++)
+//{
+	//$num = $files[$i];
+	//print $num."<br />";
+	//echo '<img src="'.$num.'" alt="random image" />'."<br /><br />";
+//}
+
+
+$sqlimage = "SELECT front FROM people;
+$imageresult1 = mysql_query($sqlimage);
+
+while($rows = mysql_fetch_assoc($imageresult1))
+{       
+    $image = $rows['image'];    
+    print $image;
 }
 ?>
